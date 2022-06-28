44a8c5e8005c0d3f69feb36b1dac743a925e3588
Gina Tipa Herrera <gitipa.uxuic2@ucamp.utel.edu.mx>
2022-06-27 21:48:59 -0500

    Inicio de app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+import Title from '../components/Title';
+import Footer from '../components/Footer';
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <Title />
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
